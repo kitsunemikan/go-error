0e5e842fad9530a073e21ff2054e59b9b9aedf1f
kitsunemikan <108350823+kitsunemikan@users.noreply.github.com>
2023-07-06 20:44:44 +0300

    error_string and comparison between error_string and error, error and error

--- src/go-error-test.main.cpp
+++ src/go-error-test.main.cpp
@@ -4,9 +4,20 @@
 
 int main()
 {
-	go::error err("test");
+	//
+	// Features:
+	// 1. Compare error (same ptr)
 
-	std::cout << err.message() << '\n';
+	go::error_string eagain("Try again later");
+	go::error_string eof("End of file");
+	go::error errEagain1(eagain);
+	go::error errEagain2(eagain);
+
+	std::cout << std::boolalpha;
+
+	std::cout << "err1 == eagain: " << (errEagain1 == eagain) << '\n';
+	std::cout << "err1 == eof: " << (errEagain1 == eof) << '\n';
+	std::cout << "err1 == err2: " << (errEagain1 == errEagain2) << '\n';
 
 	return 0;
 }
--- src/go/error.hpp
+++ src/go/error.hpp
@@ -1,23 +1,106 @@
 #pragma once
 
+#include <type_traits>
 #include <string>
+#include <memory>
 
 namespace go
 {
-
-	struct error
+	struct error_interface
 	{
-		explicit error(const char* str) :
-			msg_(str)
-		{}
+		virtual std::string message() const = 0;
+		virtual ~error_interface() noexcept = default;
+	};
 
-		std::string message() const
+	struct errorlike
+	{
+		virtual std::shared_ptr<error_interface> bare_error() const = 0;
+		virtual std::string message() const = 0;
+		virtual ~errorlike() noexcept = default;
+	};
+
+	struct error : public errorlike
+	{
+		error() = default;
+
+		template<
+			class Error,
+			class = std::enable_if<std::is_base_of_v<errorlike, Error>>::type
+		>
+		error(const Error& err)
 		{
-			return msg_;
+			err_ = err.bare_error();
+		}
+
+		std::shared_ptr<error_interface> bare_error() const override
+		{
+			return err_;
+		}
+
+		std::string message() const override
+		{
+			return err_->message();
 		}
 
 	private:
-		std::string msg_;
+		std::shared_ptr<error_interface> err_;
 	};
 
+	struct error_string : public errorlike
+	{
+		explicit error_string(const std::string& msg)
+		{
+			err_ = std::make_shared<impl>(msg);
+		}
+
+		std::string message() const override
+		{
+			return err_->message();
+		}
+
+		std::shared_ptr<error_interface> bare_error() const override
+		{
+			return err_;
+		}
+
+	private:
+		struct impl : public error_interface
+		{
+			std::string msg;
+
+			impl(const std::string& msg) :
+				msg(msg)
+			{}
+
+			std::string message() const override {
+				return msg;
+			}
+		};
+
+		std::shared_ptr<impl> err_;
+	};
+}
+
+template<
+	class ErrorA,
+	class ErrorB,
+	class = std::enable_if<
+		std::is_base_of_v<go::errorlike, ErrorA> && std::is_base_of_v<go::errorlike, ErrorB>
+	>::type
+>
+bool operator==(const ErrorA& a, const ErrorB& b)
+{
+	return a.bare_error().get() == b.bare_error().get();
+}
+
+template<
+	class ErrorA,
+	class ErrorB,
+	class = std::enable_if<
+		std::is_base_of_v<go::errorlike, ErrorA> && std::is_base_of_v<go::errorlike, ErrorB>
+	>::type
+>
+bool operator!=(const ErrorA& a, const ErrorB& b)
+{
+	return !(a == b);
 }
