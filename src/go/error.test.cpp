11169c18b9de6b4aa0e0b22648f295e43eb14146
kitsunemikan <108350823+kitsunemikan@users.noreply.github.com>
2023-08-05 13:24:51 +0300

    Moved all tests to different files except for error_code

---
+++ src/go/error.test.cpp
@@ -0,0 +1,71 @@
+#include <go/error.hpp>
+#include <go/error_string.hpp>
+
+#include <boost/ut.hpp>
+using namespace boost::ut;
+
+int main()
+{
+	"generic error"_test = [] {
+		should("error equals to the parent error") = [] {
+			go::error_string eagain("Try again later");
+			go::error err(eagain);
+
+			expect(eagain == err);
+			expect(err == eagain);
+		};
+
+		should("error doesn't equal to any concrete error other than parent") = [] {
+			go::error_string eagain("Try again later");
+			go::error err(eagain);
+
+			go::error_string eagain2("Try again later");
+
+			expect(eagain2 != err);
+			expect(err != eagain2);
+		};
+
+		should("error equals to generic errors with the same parent") = [] {
+			go::error_string eagain("Try again later");
+			go::error err(eagain);
+			go::error err2(eagain);
+
+			expect(err == err2);
+			expect(err2 == err);
+		};
+
+		should("error doesn't equal to any generic error with a different parent") = [] {
+			go::error_string eagain("Try again later");
+			go::error err(eagain);
+
+			go::error_string eagain2("Try again later");
+			go::error errOther(eagain2);
+
+			expect(err != errOther);
+			expect(errOther != err);
+		};
+
+		should("error.message() equals to the .message() of the parent") = [] {
+			auto text = "Test error";
+			go::error_string errConcrete(text);
+			go::error err(errConcrete);
+
+			expect(err.message() == errConcrete.message())
+				<< "got" << err.message() << "want" << errConcrete.message();
+		};
+
+		should("error can be ostreamed") = [] {
+			auto text = "Test error";
+			go::error_string errConcrete(text);
+			go::error err(errConcrete);
+
+			std::stringstream ss;
+			ss << err;
+
+			expect(err.message() == ss.str())
+				<< "got" << ss.str() << "want" << err.message();
+		};
+	};
+
+	return 0;
+}
---
+++ src/go/errorf.test.cpp
@@ -0,0 +1,55 @@
+#include <go/error.hpp>
+#include <go/errorf.hpp>
+
+#include <boost/ut.hpp>
+using namespace boost::ut;
+
+struct os_obj {
+	static inline const char* msg = "custom ostream";
+};
+
+std::ostream& operator<<(std::ostream& os, const os_obj& obj)
+{
+	return os << os_obj::msg;
+}
+
+int main()
+{
+	"errorf"_test = [] {
+		should("errorf with a single const char*") = [] {
+			const char* text = "hello";
+			auto err = go::errorf(text);
+
+			expect(err.message() == text) << "got" << err.message() << "want" << text;
+		};
+
+		should("errorf with a single std::string") = [] {
+			std::string text = "hello";
+			auto err = go::errorf(text);
+
+			expect(err.message() == text) << "got" << err.message() << "want" << text;
+		};
+
+		should("errorf with custom ostreamable object") = [] {
+			auto err = go::errorf(os_obj());
+
+			expect(err.message() == os_obj::msg) << "got" << err.message() << "want" << os_obj::msg;
+		};
+
+		should("errorf passes its arguments sequantially to an ostream") = [] {
+			auto err = go::errorf("the error is:", os_obj(), 42);
+
+			auto wantMsg = std::string("the error is:") + os_obj::msg + std::to_string(42);
+
+			expect(err.message() == wantMsg) << "got" << err.message() << "want" << wantMsg;
+		};
+
+		should("errorf of the same arguments return unequal errors") = [] {
+			auto err1 = go::errorf("test");
+			auto err2 = go::errorf("test");
+
+			expect(err1 != err2);
+		};
+	};
+	return 0;
+}
